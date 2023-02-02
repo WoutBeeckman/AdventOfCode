defff1bda392a67848717ae3b44a0242b6aad3f6
greenvirusred <wout_beeckman@hotmail.com>
2023-02-02 18:05:25 +0100

    First fuction that looks what shape it needs works. At least in my very limited testdata.

--- Day2/Day2Challenge2.py
+++ Day2/Day2Challenge2.py
@@ -1,5 +1,32 @@
+import re
 # Check what shape you need and return them in a list
-
+def getChosenShapes(inputFile):
+    shapes = []
+    with open(inputFile) as file:
+        for line in file:
+            if re.match(r".*X", line):
+                if re.match(r"A.*", line):
+                    shapes.append("Z")
+                elif re.match(r"B.*", line):
+                    shapes.append("X")
+                else:
+                    shapes.append("Y")
+            elif re.match(r".*Y", line):
+                if re.match(r"A.*", line):
+                    shapes.append("X")
+                elif re.match(r"B.*", line):
+                    shapes.append("Y")
+                else:
+                    shapes.append("Z")
+            elif re.match(r".*Z", line):
+                if re.match(r"A.*", line):
+                    shapes.append("Y")
+                elif re.match(r"B.*", line):
+                    shapes.append("Z")
+                else:
+                    shapes.append("X")
+    return shapes
+print(getChosenShapes("testData"))
 # Check the list with chosen shapes, and calculate points
 
 # Calculate all wins and draws
