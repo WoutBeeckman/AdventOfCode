f55f5db2414abb4af89c6e9c190acfef21d8ab52
greenvirusred <wout_beeckman@hotmail.com>
2023-02-03 15:33:56 +0100

    Initiated challenge and made the plan of attack.

---
+++ Day3/Day3Challenge1.py
@@ -0,0 +1,9 @@
+import re
+
+# Split the Rucksack in halve (two compartments so two strings)
+
+# Find which item appears in both strings (aka compartments)
+
+# Translate the item to it`s priority
+
+# Loop through all lines, split each line, find double items, translate to priority, calculate total score
