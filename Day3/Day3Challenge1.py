08452cddbc292c65bd9fc3f7621b2cd610873a79
greenvirusred <wout_beeckman@hotmail.com>
2023-02-03 16:05:19 +0100

    translateToPriority function works, there is a better way, I can feel it, but can't think of it so will have to use Google at some point.

--- Day3/Day3Challenge1.py
+++ Day3/Day3Challenge1.py
@@ -14,5 +14,14 @@ def findSameLetter(firstString, secondString):
             return letter
 
 # Translate the item to it`s priority
+def translateToPriority(letter):
+    priorityUpercase = [27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52]
+    lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    if letter.islower():
+        return lowercase.index(letter) + 1
+    else:
+        index = uppercase.index(letter)
+        return priorityUpercase[index]
 
 # Loop through all lines, split each line, find double items, translate to priority, calculate total score
